b8c160acfe26ba7d45c4dc9587fa4232d2d63083
Lucode710 <luca.venturi@aulab.it>
2023-08-17 17:11:17 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { API_BASE_URL, API_BASE_URL_NO_API } from './config'; // Importa l'URL di base
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [itemName, setItemName] = useState('');
+
+  
+
+  useEffect(() => {
+    axios.get(`${API_BASE_URL}/items`) // Utilizza l'URL di base nell'API URL
+      .then(response => setItems(response.data))
+      .catch(error => console.error(error));
+  }, []);
+
+  const handleAddItem = () => {
+    axios.post(`${API_BASE_URL}/items`, { name: itemName }) // Utilizza l'URL di base nell'API URL
+      .then(response => {
+        setItems([...items, response.data]);
+        setItemName('');
+      })
+      .catch(error => console.error(error));
+  };
+
+  axios.get(`${API_BASE_URL_NO_API}/sanctum/csrf-cookie`).then(response => {
+    console.log('butta bene');
+});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1>Lista degli elementi</h1>
+    <ul>
+      {items.map(item => (
+        <li key={item.id}>{item.name}</li>
+      ))}
+    </ul>
+    <div>
+      <input
+        type="text"
+        value={itemName}
+        onChange={e => setItemName(e.target.value)}
+      />
+      <button onClick={handleAddItem}>Aggiungi elemento</button>
     </div>
+  </div>
   );
 }
 
---
+++ src/config.js
@@ -0,0 +1,5 @@
+// config.js
+const API_BASE_URL = 'http://localhost:8000/api'; // Cambia l'URL in base alla tua configurazione del server Laravel
+const API_BASE_URL_NO_API = 'http://localhost:8000';
+
+export { API_BASE_URL, API_BASE_URL_NO_API };
